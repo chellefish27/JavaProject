da7607c0677f815b230e25436564b9da674991e6
OwenReid07 <reidoweng@gmail.com>
2025-03-29 02:57:51 -0400

    Update Frame.java
    fixed strange bug and added past order table

--- Frame.java
+++ Frame.java
@@ -4,6 +4,7 @@ import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 /*
     Author : Owen Reid
@@ -12,11 +13,14 @@ import java.awt.event.*;
     This Class creates the graphical interface for the "Sandwich Tracker" using the javax swing library
     It extends JFrame which allows itself to be referenced as the frame. For ease of creation many different
     parts will be put onto JPanels which will then be placed on to the frame as needed.
+
  */
 
 public class Frame extends JFrame implements ActionListener, TableModelListener {
+    //normal variables
     private final int FRAME_WIDTH = 1080;
     private final int FRAME_HEIGHT = 800;
+    private Customer selectedCustomer;
 
     //declaring panels and objects (such as labels etc.) that will be used
     private JPanel customerDataPanel, orderingPanel;
@@ -24,13 +28,13 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
     //Customer Data Panel components
     private JTable customerTable, pastOrderTable;
     private JLabel searchByEmail;
-    private JScrollPane customerScroll, pastOrderScroll;
+    private JScrollPane customerScroll, pastOrderScrollPane;
     private JTextField searchEmail;
     private JComboBox<String> emailEndings;
+    private DefaultTableModel orderModelTable;
 
     //Constructor for the Frame class, creates the frame when a new frame object is made
     public Frame() {
-        //set frame attributes
         this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.setResizable(false);
@@ -38,10 +42,9 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
         this.setLocationRelativeTo(null);
         this.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent event) {
-                //call save method
-                Main.writeCustomers(Main.customers);
-                //close window
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                //call save function where when implemented
                 closeWindow();
             }
         });
@@ -83,6 +86,9 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
 
                 Customer customer = (Customer) customerTable.getValueAt(row, 0);
                 String email = (String) customerTable.getValueAt(row, 1);
+
+                selectedCustomer = customer;
+                changePastOrders();
             }
         });
 
@@ -97,21 +103,40 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
 
         //Initialize searchEmail text field
         searchEmail = new JTextField();
-        searchEmail.setBounds(0, 52, 275, 35);
+        searchEmail.setBounds(0, 45, 350, 50);
         searchEmail.setFont(new Font("Arial", Font.PLAIN, 18));
 
         //Email Ending Box
         String[] endings = {"@gmail.com", "@outlook.com", "@yahoo.com"};
         emailEndings = new JComboBox<>(endings);
-        emailEndings.setBounds(275, 45, 225, 50);
+        emailEndings.setBounds(350, 45, 150, 50);
         emailEndings.setFont(new Font("Arial", Font.PLAIN, 18));
         emailEndings.setBackground(Color.white);
         emailEndings.addActionListener(this);
 
-        //initialize past orders table
-        String[] pastCol = {"Item", "Date", "Cost"};
-        Object[][] pastData;
-        DefaultTableModel pastTable = new DefaultTableModel();;
+        //initialize Past Orders Table
+        String[] pastOrderCol = {"Item", "Cost", "Date of Purchase"};
+        ArrayList<String> savedOrders = (selectedCustomer != null) ? selectedCustomer.getPastOrders() : new ArrayList<>();
+        //will change to object array to be able to use Order object when ready
+        Object[][] pastOrderData = new Object[savedOrders.size()][3];
+
+        for (int i = 0; i < savedOrders.size(); i++) {
+            //for now just works by using string input, will probably change to take in values from order class
+            // --> also will be able to get more data when clicked (using order object in first slot)
+            pastOrderData[i] = savedOrders.get(i).split(",");
+        }
+        orderModelTable = new DefaultTableModel(pastOrderData, pastOrderCol) {
+            @Override
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+        };
+        pastOrderTable = new JTable(orderModelTable);
+
+
+        //initialize past order scroll pane
+        pastOrderScrollPane = new JScrollPane(pastOrderTable);
+        pastOrderScrollPane.setBounds(550, 100, 500, 350);
 
 
         //add Elements to customerDataPanel
@@ -119,6 +144,7 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
         customerDataPanel.add(searchByEmail);
         customerDataPanel.add(searchEmail);
         customerDataPanel.add(emailEndings);
+        customerDataPanel.add(pastOrderScrollPane);
 
 
         //add customerData panel to Frame and show (Customer data panel is default window)
@@ -141,9 +167,25 @@ public class Frame extends JFrame implements ActionListener, TableModelListener 
             System.out.println(emailEndings.getSelectedItem());
         }
     }
-
+    //ran when window closes, closes Frame then terminates the program
     public void closeWindow() {
         this.dispose();
         System.exit(0);
+    }
+
+    public void changePastOrders() {
+        //check to make sure selectedCustomer exists
+        if (selectedCustomer == null) return;
+
+        //remove all current rows
+        for (int i = orderModelTable.getRowCount()-1; i >= 0; i--) {
+            orderModelTable.removeRow(i);
+        }
+
+        //replace rows with selected customer past orders
+        for (int i = 0; i < selectedCustomer.getPastOrders().size(); i++) {
+            Object[] data = selectedCustomer.getPastOrders().get(i).split(",");
+            orderModelTable.addRow(new Object[] {data[0], data[1], data[2]});
+        }
     }
 }
