beaaaa3df108ee8f672f92180c20eebde40624d6
OwenReid07 <reidoweng@gmail.com>
2025-03-29 01:36:04 -0400

    Update Customer.java
    Changed to array input

--- Customer.java
+++ Customer.java
@@ -1,20 +1,21 @@
 public class Customer {
-    private String name;
-    private String email;
-
-    public Customer(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    @Override
-    public String toString() {
-        return name;
-    }
-}
+	   private String name;
+	   private String email;
+	   public Customer(String[] inputArr) {
+	       this.name = inputArr[0];
+	       this.email = inputArr[1];
+	   }
+	   public String getName() {
+	       return name;
+	   }
+	   public String getEmail() {
+	       return email;
+	   }
+	   @Override
+	   public String toString() {
+	       return name;
+	   }
+	   public String customerString() {
+		   return(""+this.name+","+this.email);
+	   }
+	}
